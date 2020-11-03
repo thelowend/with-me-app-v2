58e2c64ef4eb73f5cb63a48b3f6bee61b7a2babf
thelowend <dmpablos@gmail.com>
2020-11-02 21:25:01 -0300

    Slight tweaks

--- App/App.js
+++ App/App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import OneSignal from 'react-native-onesignal';
+import { Config } from 'App/Config';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import createStore from 'App/Stores';
@@ -14,7 +15,7 @@ export default class App extends Component {
   constructor(properties) {
     super(properties);
     // OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG)
-    OneSignal.init('f9adf46b-d1d5-463f-8858-b934ef8d908c');
+    OneSignal.init(Config.ONESIGNAL_KEY);
 
     OneSignal.addEventListener('received', this.onReceived);
     OneSignal.addEventListener('opened', this.onOpened);
@@ -29,14 +30,9 @@ export default class App extends Component {
 
   onReceived(notification) {
     console.log('Notification received: ', notification);
-    // TODO: Link to contact user
   }
 
   onOpened(openResult) {
-    //console.log('Message: ', openResult.notification.payload.body)
-    //console.log('Data: ', openResult.notification.payload.additionalData)
-    //console.log('isActive: ', openResult.notification.isAppInFocus)
-    //console.log('openResult: ', openResult)
     const contact = {
       user_id: openResult.notification.payload.additionalData.user_id,
       feed: JSON.parse(openResult.notification.payload.additionalData.feed)
